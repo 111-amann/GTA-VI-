225660988ceb8713a281353f0280b714250844c8
111-amann <amansayyad7887@gmail.com>
2025-05-13 09:31:46 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GTA-VI-Website/',
+  base: '/GTA-VI-/',
   plugins: [react(), tailwindcss()],
 })
